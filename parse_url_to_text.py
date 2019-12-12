b4ca64e8acdef648cc9524bd994b6a205a31c6e2
Jouni Lammi <jjj.lammi@gmail.com>
2019-12-12 13:49:49 +0200

    update parse_url_to_text.py

--- parse_url_to_text.py
+++ parse_url_to_text.py
@@ -7,6 +7,10 @@ nlp = spacy.load("en_core_web_sm")
 
 
 def _get_html_file(url):
+    """
+    Gets a HTML version of the web page given in the 'url' argument.
+    Returns HTML version as a string.
+    """
     if not url.startswith("http"):
         url = "https://" + url
     res = requests.get(url)
@@ -17,6 +21,10 @@ def _get_html_file(url):
 
 
 def _html_to_text(html):
+    """
+    Parses HTML page to text. Returns a string.
+    """
+
     # soup = BeautifulSoup(html, features="html.parser")
     # # text = soup.find_all(text=True)
     # blacklist = [
@@ -31,7 +39,7 @@ def _html_to_text(html):
     #     'header',
     #     # 'html',
     #     'meta',
-    #     'head', 
+    #     'head',
     #     'input',
     #     'script',
     # ]
@@ -58,8 +66,13 @@ def _html_to_text(html):
     h.ignore_links = True
     h.ignore_images = True
     return h.handle(html)
-    
+
+
 def _parse_body_text_from_text_version(text):
+    """
+    Takes the full text and removes clutter. Returns parsed text
+    """
+
     text_as_list = text.split("\n")
     for idx, row in enumerate(text_as_list):
         if "## related topics" in row.lower():
@@ -92,7 +105,7 @@ def _parse_body_text_from_text_version(text):
         if "Close share panel" in row:
             text_as_list = text_as_list[idx+3:]
             break
-    
+
     # print(text_as_list)
     print("\n".join(text_as_list))
     text = "\n".join(text_as_list)
@@ -101,19 +114,32 @@ def _parse_body_text_from_text_version(text):
 
 
 def parse_body_text_from_url(link):
+    """
+    Fetches the HTML version of a BBC page and parses it to text.
+    """
+
     html = _get_html_file(link)
     text = _html_to_text(html)
     text = _parse_body_text_from_text_version(text)
+    return text
+
+
+def _debugfunc():
+    """
+    Contains links for BBC articles and printing. Not to use in
+    production.
+    """
+
+    # link = "https://www.bbc.com/news/world-asia-50723352"
+    # link = "https://www.bbc.com/news/live/election-2019-50739883"
+    # link = "https://www.bbc.com/news/world-us-canada-50747374"
+    # link = "https://www.bbc.com/news/world-asia-50741094"
+    link = "https://www.bbc.com/news/world-europe-50740324"
+    # link = "https://www.iltalehti.fi/viihdeuutiset/a/045cb810-1ffd-4641-84d7-4995953a9a4d"
+    text = parse_body_text_from_url(link)
+    print(text)
 
 
 
-# link = "https://www.bbc.com/news/world-asia-50723352"
-# link = "https://www.bbc.com/news/live/election-2019-50739883"
-# link = "https://www.bbc.com/news/world-us-canada-50747374"
-# link = "https://www.bbc.com/news/world-asia-50741094"
-# link = "https://www.bbc.com/news/world-europe-50740324"
-# link = "https://www.iltalehti.fi/viihdeuutiset/a/045cb810-1ffd-4641-84d7-4995953a9a4d"
 
-# parse_body_text_from_url(link)
 
-# print(text)
