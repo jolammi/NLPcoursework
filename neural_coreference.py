4089dea1945c418f463bff07252b7c0d9fd0e03d
Jouni Lammi <jjj.lammi@gmail.com>
2020-01-06 16:49:33 +0200

    Update neural_coreference.py

--- neural_coreference.py
+++ neural_coreference.py
@@ -23,7 +23,7 @@ from parse_url_to_text import parse_body_text_from_url
 def neural_coreference(text):
     nlp = spacy.load('en_core_web_sm')
     neuralcoref.add_to_pipe(nlp)
-    
+
     ##doc1 = "Students are hungry but they feel the need for speed."
     ##doc1 = "Students are hungry but they feel the need for speed. Students really like John. John is a sick bastard and he is white."
     ##doc1 = "The 16-year-old is the youngest person to be chosen by the magazine in a tradition that started in 1927. Speaking at a UN climate change summit in Madrid before the announcement, she urged world leaders to stop using creative PR to avoid real action. The next decade would define the planet's future, she said. Last year, the teenager started an environmental strike by missing lessons most Fridays to protest outside the Swedish parliament building. It sparked a worldwide movement that became popular with the hashtag #FridaysForFuture. Since then, she has become a strong voice for action on climate change, inspiring millions of students to join protests around the world. Earlier this year, she was nominated as a candidate for the Nobel Peace Prize."
@@ -37,8 +37,11 @@ def neural_coreference(text):
         output += "\n"
     return output
 
-# link = "https://www.bbc.com/news/world-europe-50740324"
-# neural_coreference()
+# ------- comment before pushing, debug
+link = "https://www.bbc.com/news/world-europe-50740324"
+doc1 = parse_body_text_from_url(link)
+neural_coreference(doc1)
+# ///////// comment before pushing
 
 
 ##for ent in doc1.ents:
